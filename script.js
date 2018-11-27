6cc8828e2d106b8773aec541167475075b757d78
Bernardo Amaral <bernardoamaral1011@gmail.com>
2018-11-27 01:31:05 +0000

    cav - Master
    Website only requires the remaining content. Footer is not fixed in unfilled pages but will fix with the content. Website final stage.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+    
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "260px";
+    document.getElementById("main").style.marginLeft = "260px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
